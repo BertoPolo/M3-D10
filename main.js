3c9e541632f5c06bd878e9f1a808e5fc880a92d1
BertoPolo <bertopolo91@gmail.com>
2022-02-15 17:54:35 +0100

    xgfsdfg

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 /* 
-  fetch("https://striveschool-api.herokuapp.com/api/movies", {
-    method: "POST",
-    body: JSON.stringify(genres),
-    headers: new Headers({
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNmFlMDgyZWExZDAwMTViYjA0MzciLCJpYXQiOjE2NDM4MDEzMTIsImV4cCI6MTY0NTAxMDkxMn0.dlz2qOuGVhBvUctJZ6tPWRv6oLf_d1KGnXfKXwMn6Ys",
-      "Content-Type": "application/json",
-    }),
-  }).catch((err) => console.log(err));
-  console.log(genres);
-
-  alert("the genre has been created, lets g0000");
-};
+  
 
 /* fetch("https://striveschool-api.herokuapp.com/api/movies/" + genres, {
 
@@ -52,16 +40,31 @@ window.onload = function () {
  */
 
 const handleSubmit = function (event) {
-    event.preventDefault();
-  
-    const movieInput = {
-      name: document.getElementById("inputName").value,
-      description: document.getElementById("inputDescription").value,
-      category: document.getElementById("inputCategory").value,
-      imageUrl: document.getElementById("inputImgeUrl").value,
-    };
-  
+  event.preventDefault();
 
+  const movieInput = {
+    name: document.getElementById("inputName").value,
+    description: document.getElementById("inputDescription").value,
+    category: document.getElementById("inputCategory").value,
+    imageUrl: document.getElementById("inputImgeUrl").value,
+  };
+
+  // here put the POST
+
+  /* fetch("https://striveschool-api.herokuapp.com/api/movies", {
+    method: "POST",
+    body: JSON.stringify(genres),
+    headers: new Headers({
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNmFlMDgyZWExZDAwMTViYjA0MzciLCJpYXQiOjE2NDM4MDEzMTIsImV4cCI6MTY0NTAxMDkxMn0.dlz2qOuGVhBvUctJZ6tPWRv6oLf_d1KGnXfKXwMn6Ys",
+      "Content-Type": "application/json",
+    }),
+  }).catch((err) => console.log(err));
+  console.log(genres);
+
+  alert("the genre has been created, lets g0000");
+}; */
+};
 const genresCreator = function (genres) {
   const dropdown = document.getElementById("genresDropdown");
   genres.forEach(
@@ -91,4 +94,5 @@ const fectcherMovies = async () => {
     console.log(error);
   }
 };
+
 fectcherMovies();
